91ccc7d6b78789459f1b66f2cbe93fda32562048
Camilo Lopes <camilosi@hotmail.com>
2012-03-17 23:04:00 -0300

    validando mudanças.

--- BuilderBoleto/src/com/camilolopes/interfaces/Boleto.java
+++ BuilderBoleto/src/com/camilolopes/interfaces/Boleto.java
@@ -5,7 +5,7 @@ import java.util.Calendar;
  * interface que define o que um boleto 
  * deve ter. qualquer banco quer tem interesse em gerar
  * um boleto deve informar quem é o sacado, cedente 
- * o prazo para pagto e o valor. 
+ * o prazo para pagto e o valor
  */
 public interface Boleto {
 	String getSacado();	
